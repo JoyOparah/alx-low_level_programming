6a6d7c2bc3e4f9f75d4f92e91655ac62e2f0d572
JoyOparah <oparahjoy@gmail.com>
2022-04-08 16:17:39 -0700

    commit 5

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alp[24] = "abcdfghijklmnoprstuvwxyz";
+	int e;
+
+	for (e = 0; e < 24; e++)
+	{
+		putchar(alp[e]);
+	}
+	putchar('\n');
+	return (0);
+			
+}
