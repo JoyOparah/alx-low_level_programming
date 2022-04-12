795be2451d6572ee492a51c454cfbe37391eb039
JoyOparah <oparahjoy@gmail.com>
2022-04-12 16:40:50 -0700

    commit 2

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+void print_alphabet(void)
+{
+	for (b = 97; b <= 122; b++)
+	{
+		_putchar(b);
+	}
+	_putchar('\n');
+}
