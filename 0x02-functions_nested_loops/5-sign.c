b824feb05b63f315b392bf2e74e36e757a12e95c
JoyOparah <oparahjoy@gmail.com>
2022-04-12 18:52:44 -0700

    commit 6

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ *
+ * print_sign - check runners
+ *@n : number to check
+ * Return: Always 0 (Success)
+ */
+int print_sign(int n)
+{
+	int test;
+
+	if (n > 0)
+	{
+		test = 1;
+		_putchar('+');
+	}
+	else if (n == 0)
+	{
+		test = 0;
+		_putchar('0');
+	}
+	else
+	{
+		test = -1;
+		_putchar('-');
+	}
+	return (test);
+}
