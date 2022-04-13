02a5f6afe7ac388339f20fde367ca1266c71b17a
JoyOparah <oparahjoy@gmail.com>
2022-04-12 17:10:43 -0700

    commit 3

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * 
+ *
+ * Return: Always 0 (Success)
+ */
+void print_alphabet_x10(void)
+{
+	int b;
+	int c;
+
+	for (b = 0; b < 10; b++)
+	{
+		for (c = 97; c <= 122; c++)
+		{
+			_putchar(c);
+		}
+		_putchar('\n');
+
+	}
+}
